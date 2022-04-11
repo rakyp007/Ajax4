50880bc9239f9ed8c1ff81d5547c0f0de3859c3c
Your Name <rusikrusikov973@gmail.com>
2022-04-10 17:06:40 -0700

    dz

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
-
-
 const requestURL = 'https://jsonplaceholder.typicode.com/photos'
-
 const userNameBtn = document.getElementById('userName')
-
-
-
-
 
 function userName(URL){
     const xhr = new XMLHttpRequest()
@@ -21,27 +14,24 @@ xhr.onload = () => {
     else{
         console.log(xhr.response);
     }
-
 }
 xhr.onerror=() =>{
     console.log(xhr.response);
 }
 xhr.send()
 }
-
-userNameBtn.addEventListener('click', (e) =>{
+userNameBtn.addEventListener('click', (e) => {
     e.preventDefault();
      userName(requestURL)
 })
 
+
 const requestUrL = 'https://jsonplaceholder.typicode.com/photos'
 const mirBtn = document.getElementById('mir')
-const ul = document.getElementById('class')
 
-function mir(URL){
+function mir(URL) {
     const xhr = new XMLHttpRequest()
     xhr.responseType = 'json'
-
 
 let users = {
     name: 'rakyp',
@@ -49,25 +39,21 @@ let users = {
     position: "Frontend"
 }
 
-
 xhr.open('POST',requestUrL)
-
 
 xhr.onload = () => {
     if(xhr.status >= 400){
         console.log('error',xhr.status);
-    }else{
+    } else{
         console.log(xhr.response);
     }
-
 }
 
 const data = JSON.stringify(users)
 xhr.send(data)
 }
 
-
-mirBtn.addEventListener('click',(e) => {
+mirBtn.addEventListener('click', (e) => {
     e.preventDefault();
     mir(requestUrL)
 });
